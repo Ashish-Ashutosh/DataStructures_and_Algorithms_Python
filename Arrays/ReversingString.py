dec3c1d9ce8ae3d738b015fc119715e767856153
Ashish-Ashutosh <ashish.a.may@gmail.com>
2020-08-01 21:14:45 +0200

    - implemented multiple ways of reversing a string

---
+++ Arrays/ReversingString.py
@@ -0,0 +1,26 @@
+def reverseString(input):
+    #first we do some validation to check if the input is a STRING
+    if(input.isnumeric() or len(input) < 2 ):
+        print("Invalid input type. Please enter a string")
+    else:
+        reverseString = []
+        i=len(input)
+
+        while(i>0):
+            #storing the last element of input string into a LIST (Note that we reduce index by 1 because "string" count starts from index = 1
+            reverseString.append(input[i-1])
+            i=i-1
+
+        #converting a LIST to String using 'join'
+        print(''.join(reverseString))
+
+
+def reverseString2(input):
+    outputString = input[::-1]
+    print(outputString)
+
+
+
+#method call to reverse a string
+reverseString("ashish")
+reverseString2("ashish")
